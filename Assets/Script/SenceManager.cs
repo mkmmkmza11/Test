735eb8d6721809df567ca5e6c772f0b69884498e
mkmmkmza11 <thipatai11@gmail.com>
2022-07-05 10:22:46 +0700

    Test merge

--- Assets/Script/SenceManager.cs
+++ Assets/Script/SenceManager.cs
@@ -18,15 +18,6 @@ public class SenceManager : MonoBehaviour
     }
     
 
-    // Update is called once per frame
-    /*void Update()
-    {
-       
-        if (Input.touchCount > 0)
-        {
-            LoadSceneGame(NameScene);
-        }
-    }*/
 
 
     public IEnumerator SceneStart(string sceneName)
